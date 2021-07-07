7026080e1cdd4d301749875dfa0eab8103c084b3
kombii <377981654@qq.com>
2021-07-07 14:29:33 +0800

    修正spiselpin 手滑删了

--- HARDWARE/ICOPYX_BSP/ICOPYX_BSP.h
+++ HARDWARE/ICOPYX_BSP/ICOPYX_BSP.h
@@ -28,113 +28,111 @@
 #define BLAddress 0x0800B400
 
 ///////////////////////////////////////////GPIO DEFINE START///////////////////			  
-	#define WKUP_Pin 							GPIO_Pin_0
-	#define WKUP_GPIO_Port 						GPIOA 
+#define WKUP_Pin 							GPIO_Pin_0
+#define WKUP_GPIO_Port 						GPIOA 
 
-	#define VUSB_Pin  							GPIO_Pin_1
-	#define VUSB_GPIO_Port 						GPIOA 
+#define VUSB_Pin  							GPIO_Pin_1
+#define VUSB_GPIO_Port 						GPIOA 
 
-	#define VBAT_Pin 							GPIO_Pin_2
-	#define VBAT_GPIO_Port 						GPIOA 
+#define VBAT_Pin 							GPIO_Pin_2
+#define VBAT_GPIO_Port 						GPIOA 
 
-	#define BATSAMP_Pin 						GPIO_Pin_11
-	#define BATSAMP_GPIO_Port 					GPIOB
+#define BATSAMP_Pin 						GPIO_Pin_11
+#define BATSAMP_GPIO_Port 					GPIOB
 			
-	#define OTGSENSE_Pin 						GPIO_Pin_15
-	#define OTGSENSE_GPIO_Port 					GPIOB
+#define OTGSENSE_Pin 						GPIO_Pin_15
+#define OTGSENSE_GPIO_Port 					GPIOB
 
-	#define CHARG_EN_Pin 						GPIO_Pin_12
-	#define CHARG_EN_GPIO_Port 					GPIOB
+#define CHARG_EN_Pin 						GPIO_Pin_12
+#define CHARG_EN_GPIO_Port 					GPIOB
 
-	#define AMP_STDN_Pin						GPIO_Pin_6
-	#define AMP_STDN_GPIO_Port					GPIOB	
+#define AMP_STDN_Pin						GPIO_Pin_6
+#define AMP_STDN_GPIO_Port					GPIOB	
 
-	#define SWDIO_Pin 							GPIO_Pin_13
-	#define SWDIO_GPIO_Port 					GPIOA 
-	#define SWCLK_Pin 							GPIO_Pin_14
-	#define SWCLK_GPIO_Port 					GPIOA 
+#define SWDIO_Pin 							GPIO_Pin_13
+#define SWDIO_GPIO_Port 					GPIOA 
+#define SWCLK_Pin 							GPIO_Pin_14
+#define SWCLK_GPIO_Port 					GPIOA 
 												  											  
-	#define BUTRIGHT_Pin 						GPIO_Pin_12
-	#define BUTRIGHT_GPIO_Port 					GPIOA 
-	#define BUTUP_Pin 							GPIO_Pin_15
-	#define BUTUP_GPIO_Port 					GPIOA 
-	#define BUTOK_Pin 							GPIO_Pin_3
-	#define BUTOK_GPIO_Port 					GPIOB 
-	#define BUTDOWN_Pin 						GPIO_Pin_4
-	#define BUTDOWN_GPIO_Port 					GPIOB 
-	#define BUTLEFT_Pin 						GPIO_Pin_5
-	#define BUTLEFT_GPIO_Port 					GPIOB 
-	#define BUTM1_Pin 							GPIO_Pin_13
-	#define BUTM1_GPIO_Port			 			GPIOA 
-	#define BUTM2_Pin 							GPIO_Pin_11
-	#define BUTM2_GPIO_Port 					GPIOA 
-	#define BUTPWR_CAN_Pin 						GPIO_Pin_14
-	#define BUTPWR_CAN_GPIO_Port 				GPIOA 
-	#define BUTALL_Pin 							GPIO_Pin_10
-	#define BUTALL_GPIO_Port 					GPIOB 
+#define BUTRIGHT_Pin 						GPIO_Pin_12
+#define BUTRIGHT_GPIO_Port 					GPIOA 
+#define BUTUP_Pin 							GPIO_Pin_15
+#define BUTUP_GPIO_Port 					GPIOA 
+#define BUTOK_Pin 							GPIO_Pin_3
+#define BUTOK_GPIO_Port 					GPIOB 
+#define BUTDOWN_Pin 						GPIO_Pin_4
+#define BUTDOWN_GPIO_Port 					GPIOB 
+#define BUTLEFT_Pin 						GPIO_Pin_5
+#define BUTLEFT_GPIO_Port 					GPIOB 
+#define BUTM1_Pin 							GPIO_Pin_13
+#define BUTM1_GPIO_Port			 			GPIOA 
+#define BUTM2_Pin 							GPIO_Pin_11
+#define BUTM2_GPIO_Port 					GPIOA 
+#define BUTPWR_CAN_Pin 						GPIO_Pin_14
+#define BUTPWR_CAN_GPIO_Port 				GPIOA 
+#define BUTALL_Pin 							GPIO_Pin_10
+#define BUTALL_GPIO_Port 					GPIOB 
 												  
-	#define H3_PWR_ON_OFF_Pin 					GPIO_Pin_8
-	#define H3_PWR_ON_OFF_GPIO_Port 			GPIOB 
-	#define PM_PWR_ON_OFF_Pin 					GPIO_Pin_9
-	#define PM_PWR_ON_OFF_GPIO_Port 			GPIOB 
+#define H3_PWR_ON_OFF_Pin 					GPIO_Pin_8
+#define H3_PWR_ON_OFF_GPIO_Port 			GPIOB 
+#define PM_PWR_ON_OFF_Pin 					GPIO_Pin_9
+#define PM_PWR_ON_OFF_GPIO_Port 			GPIOB 
 
-	#define Q25_EN_Pin 						    GPIO_Pin_2
-	#define Q25_EN_GPIO_Port 					GPIOB 
+#define Q25_EN_Pin 						    GPIO_Pin_2
+#define Q25_EN_GPIO_Port 					GPIOB 
 
-	#define VUSB_Pin  							GPIO_Pin_1
-	#define VUSB_GPIO_Port 						GPIOA 
+#define SPI_SEL_Pin 						GPIO_Pin_13
+#define SPI_SEL_GPIO_Port 					GPIOB 
 												  
-	#define PM_BUTTON_Pin 						GPIO_Pin_7
-	#define PM_BUTTON_GPIO_Port 				GPIOB
+#define PM_BUTTON_Pin 						GPIO_Pin_7
+#define PM_BUTTON_GPIO_Port 				GPIOB
 												  
-	#define PM_4LED_Pin 						GPIO_Pin_14
-	#define PM_4LED_GPIO_Port 					GPIOB
+#define PM_4LED_Pin 						GPIO_Pin_14
+#define PM_4LED_GPIO_Port 					GPIOB
 												 
-	#define LCD_DC_Pin 							GPIO_Pin_3
-	#define LCD_DC_GPIO_Port 					GPIOA
-	#define LCD_CS_Pin 							GPIO_Pin_4
-	#define LCD_CS_GPIO_Port 					GPIOA
-	#define LCD_SCL_Pin 						GPIO_Pin_5
-	#define LCD_SCL_GPIO_Port 					GPIOA
-	#define LCD_RST_Pin 						GPIO_Pin_0
-	#define LCD_RST_GPIO_Port 					GPIOB
-	#define LCD_SDA_Pin 						GPIO_Pin_7
-	#define LCD_SDA_GPIO_Port 					GPIOA
-	#define LCD_PWR_Pin 						GPIO_Pin_1
-	#define LCD_PWR_GPIO_Port 					GPIOB
-	#define LCD_BKLT_Pin 						GPIO_Pin_8
-	#define LCD_BKLT_GPIO_Port 					GPIOA
+#define LCD_DC_Pin 							GPIO_Pin_3
+#define LCD_DC_GPIO_Port 					GPIOA
+#define LCD_CS_Pin 							GPIO_Pin_4
+#define LCD_CS_GPIO_Port 					GPIOA
+#define LCD_SCL_Pin 						GPIO_Pin_5
+#define LCD_SCL_GPIO_Port 					GPIOA
+#define LCD_RST_Pin 						GPIO_Pin_0
+#define LCD_RST_GPIO_Port 					GPIOB
+#define LCD_SDA_Pin 						GPIO_Pin_7
+#define LCD_SDA_GPIO_Port 					GPIOA
+#define LCD_PWR_Pin 						GPIO_Pin_1
+#define LCD_PWR_GPIO_Port 					GPIOB
+#define LCD_BKLT_Pin 						GPIO_Pin_8
+#define LCD_BKLT_GPIO_Port 					GPIOA
 
-	#define BATRESNET							(1.333333333333333)
-	#define VCCRESNET							(1.666666666666667)
-	#define REFVOL								(3.301)
-	#define INTREFVOL							(1.200)
+#define BATRESNET							(1.333333333333333)
+#define VCCRESNET							(1.666666666666667)
+#define REFVOL								(3.301)
+#define INTREFVOL							(1.200)
 
-	#define Intvolavl	((u16)(Get_Adc_Average(ADC_Channel_17, 1)))
+#define Intvolavl	((u16)(Get_Adc_Average(ADC_Channel_17, 1)))
 
 		//#define VCCvolavlNOSE   ((u16)(Get_Adc_Average(ADC_Channel_1, 2)*(REFVOL / 4096) * 1000 * VCCRESNET))
 		//#define BATvolavlNOSE   ((u16)(Get_Adc_Average(ADC_Channel_2, 2)*(REFVOL / 4096) * 1000 * BATRESNET))
-	#define VCCvolavl   ((u16)(Get_Adc_Average(ADC_Channel_1, 2)*(INTREFVOL / Intvolavl) * 1000 * VCCRESNET))
-	#define BATvolavl   ((u16)(Get_Adc_Average(ADC_Channel_2, 100)*(INTREFVOL / Intvolavl) * 1000 * BATRESNET))
+#define VCCvolavl   ((u16)(Get_Adc_Average(ADC_Channel_1, 2)*(INTREFVOL / Intvolavl) * 1000 * VCCRESNET))
+#define BATvolavl   ((u16)(Get_Adc_Average(ADC_Channel_2, 100)*(INTREFVOL / Intvolavl) * 1000 * BATRESNET))
 
 		//#define VCCvolNOSE   ((u16)(Get_Adc(1)*(REFVOL / 4096) * 1000 * VCCRESNET))
 		//#define BATvolNOSE   ((u16)(Get_Adc(2)*(REFVOL / 4096) * 1000 * BATRESNET))
-	#define VCCvol   ((u16)(Get_Adc(1)*(INTREFVOL / Intvolavl) * 1000 * VCCRESNET))
+#define VCCvol   ((u16)(Get_Adc(1)*(INTREFVOL / Intvolavl) * 1000 * VCCRESNET))
 		//#define BATvol   ((u16)(Get_Adc(2)*(INTREFVOL / Intvolavl) * 1000 * BATRESNET))
 
-	#define turnoffpm3()	GPIO_SetBits(PM_PWR_ON_OFF_GPIO_Port, PM_PWR_ON_OFF_Pin)
-	#define turnoffh3()		GPIO_SetBits(H3_PWR_ON_OFF_GPIO_Port, H3_PWR_ON_OFF_Pin)
-	#define turnonpm3()		GPIO_ResetBits(PM_PWR_ON_OFF_GPIO_Port, PM_PWR_ON_OFF_Pin)
-	#define turnonh3()		GPIO_ResetBits(H3_PWR_ON_OFF_GPIO_Port, H3_PWR_ON_OFF_Pin)
+#define turnoffpm3()	GPIO_SetBits(PM_PWR_ON_OFF_GPIO_Port, PM_PWR_ON_OFF_Pin)
+#define turnoffh3()		GPIO_SetBits(H3_PWR_ON_OFF_GPIO_Port, H3_PWR_ON_OFF_Pin)
+#define turnonpm3()		GPIO_ResetBits(PM_PWR_ON_OFF_GPIO_Port, PM_PWR_ON_OFF_Pin)
+#define turnonh3()		GPIO_ResetBits(H3_PWR_ON_OFF_GPIO_Port, H3_PWR_ON_OFF_Pin)
 
-	#define turnonchg()		GPIO_ResetBits(CHARG_EN_GPIO_Port, CHARG_EN_Pin)
-	#define turnoffchg()	GPIO_SetBits(CHARG_EN_GPIO_Port, CHARG_EN_Pin)
-	#define readchg()       GPIO_ReadOutputDataBit(CHARG_EN_GPIO_Port, CHARG_EN_Pin)
+#define turnonchg()		GPIO_ResetBits(CHARG_EN_GPIO_Port, CHARG_EN_Pin)
+#define turnoffchg()	GPIO_SetBits(CHARG_EN_GPIO_Port, CHARG_EN_Pin)
+#define readchg()       GPIO_ReadOutputDataBit(CHARG_EN_GPIO_Port, CHARG_EN_Pin)
 
-	#define SPISELH3()		GPIO_SetBits(SPI_SEL_GPIO_Port, SPI_SEL_Pin)
-	#define SPISELST()		GPIO_ResetBits(SPI_SEL_GPIO_Port, SPI_SEL_Pin)
-
-
+#define SPISELH3()		GPIO_SetBits(SPI_SEL_GPIO_Port, SPI_SEL_Pin)
+#define SPISELST()		GPIO_ResetBits(SPI_SEL_GPIO_Port, SPI_SEL_Pin)
 
 #define START_MODE_NONE		0
 #define START_MODE_BAT		1
