5c3df59f8f92b2f51508b0b3a43df44397af9217
kombii <377981654@qq.com>
2021-09-23 14:21:31 +0800

    添加了按钮按下和抬起的单独指令

--- SYSTEM/COMMPROC/sys_command_line.c
+++ SYSTEM/COMMPROC/sys_command_line.c
@@ -10,6 +10,8 @@ static uint8_t cli_showsimbol(void *para, uint8_t len);
 static uint8_t cli_showstring(void *para, uint8_t len);
 static uint8_t cli_showpicture(void *para, uint8_t len);
 static uint8_t cli_presspm3(void *para, uint8_t len);
+static uint8_t cli_presspm3on(void *para, uint8_t len);
+static uint8_t cli_presspm3off(void *para, uint8_t len);
 static uint8_t cli_ledpm3(void *para, uint8_t len);
 static uint8_t cli_turnoffpm3(void *para, uint8_t len);
 static uint8_t cli_turnonpm3(void *para, uint8_t len);
@@ -56,6 +58,8 @@ const COMMAND_S CLI_Cmd[] = {
 	//pm3状态操作指令序列
 	{ "ledpm3", NULL, NULL, cli_ledpm3},
 	{ "presspm3", NULL, NULL, cli_presspm3},
+	{ "butonpm3", NULL, NULL, cli_presspm3on},
+	{ "butoffpm3", NULL, NULL, cli_presspm3off},
 	//pm3电源指令序列
 	{ "turnonpm3", NULL, NULL, cli_turnonpm3},
 	{ "turnoffpm3", NULL, NULL, cli_turnoffpm3},
@@ -182,6 +186,16 @@ static uint8_t cli_presspm3(void *para, uint8_t len)
 	{
 		GPIO_SetBits(PM_BUTTON_GPIO_Port, PM_BUTTON_Pin);
 	}
+	GPIO_ResetBits(PM_BUTTON_GPIO_Port, PM_BUTTON_Pin);
+	return TRUE;
+}
+//按下pm3按钮实现
+static uint8_t cli_presspm3on(void *para, uint8_t len) {
+	GPIO_SetBits(PM_BUTTON_GPIO_Port, PM_BUTTON_Pin);
+	return TRUE;
+}
+//抬起pm3按钮实现
+static uint8_t cli_presspm3off(void *para, uint8_t len) {
 	GPIO_ResetBits(PM_BUTTON_GPIO_Port, PM_BUTTON_Pin);
 	return TRUE;
 }
